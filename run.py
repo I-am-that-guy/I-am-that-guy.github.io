0d9783cfa360739be0f61ee3cad243ea2e0def38
Jolaoluwa Oladipupo Amodu <jola47amodu@gmail.com>
2026-09-14 14:58:46 -0500

    New Portfolio Website

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 from flaskblog import app
-from flaskblog.models import create_posts_table
+from flaskblog.models import create_posts_table, create_documents_table
 
 if __name__ == '__main__':
 
     with app.app_context():
         create_posts_table()
+        try:
+            create_documents_table()
+        except Exception as exc:  # the site must still boot if the DB hiccups
+            print(f"[warn] could not create documents table: {exc}")
 
     app.run(debug=True)
